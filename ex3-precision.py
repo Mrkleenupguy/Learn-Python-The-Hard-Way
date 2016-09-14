a5201c94bb7a3d6ae996ecdf498a8843876e8030
John Radcliffe <Mrkleenupguy@users.noreply.github.com>
2016-09-14 13:29:40 -0700

    Added Exercise 3 and 3.1

---
+++ ex3-precision.py
@@ -0,0 +1,27 @@
+print "I will now count my chickens:"
+# Add 25 to (30 divided by 6)
+print "Hens", 25.0 + 30.0 / 6.0
+# Subtract ((25 multiplied by 3) modulus 4) from 100
+print "Roosters", 100.0 - 25.0 * 3.0 % 4.0
+
+print "Now I will count the eggs:"
+# Add 3 to 2 to 1 subtract 5 plus (4 mod 2) minus (1 divded by 4) plus 6
+print 3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0
+
+print "Is it true that 3 + 2 < 5 - 7?"
+# Test if 3 plus 2 is less than 5 minus 7
+print 3.0 + 2.0 < 5.0 - 7.0
+# Add 3 to 2
+print "What is 3 + 2?", 3.0 + 2.0
+# Minus 7 from 5
+print "What is 5 - 7?", 5.0 - 7.0
+
+print "Oh, that's why it's False."
+
+print "How about some more."
+#T est if 5 is greater than -2
+print "Is it greater?", 5.0 > -2.0
+# Test if 5 is greater than or equal to -2
+print "Is it greater or equal?", 5.0 >= -2.0
+# Test if 5 is less than or equal to -2
+print "Is it less or equal?", 5.0 <= -2.0
---
+++ ex3.py
@@ -0,0 +1,27 @@
+print "I will now count my chickens:"
+# Add 25 to (30 divided by 6)
+print "Hens", 25 + 30 / 6
+# Subtract ((25 multiplied by 3) modulus 4) from 100
+print "Roosters", 100 - 25 * 3 % 4
+
+print "Now I will count the eggs:"
+# Add 3 to 2 to 1 subtract 5 plus (4 mod 2) minus (1 divded by 4) plus 6
+print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+print "Is it true that 3 + 2 < 5 - 7?"
+# Test if 3 plus 2 is less than 5 minus 7
+print 3 + 2 < 5 - 7
+# Add 3 to 2
+print "What is 3 + 2?", 3 + 2
+# Minus 7 from 5
+print "What is 5 - 7?", 5 - 7
+
+print "Oh, that's why it's False."
+
+print "How about some more."
+#T est if 5 is greater than -2
+print "Is it greater?", 5 > -2
+# Test if 5 is greater than or equal to -2
+print "Is it greater or equal?", 5 >= -2
+# Test if 5 is less than or equal to -2
+print "Is it less or equal?", 5 <= -2
